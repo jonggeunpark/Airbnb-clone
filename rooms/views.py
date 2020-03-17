251fb186cd85f6b10e787b6f52d81bef3170e79c
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2020-03-17 23:56:40 +0900

    add templates for base, homepage

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from rooms import models as rooms_models
 
-# Create your views here.
+
+def all_rooms(request):
+    all_rooms = rooms_models.Room.objects.all()
+    return render(request, "rooms/home.html", context={"rooms": all_rooms})
