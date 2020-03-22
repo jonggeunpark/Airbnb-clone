f080138e5f78117a3fc447999e3f313897ee474a
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2020-03-22 23:14:39 +0900

    change Room Detail from fbv to cbv

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,4 @@
-from django.utils import timezone
-from django.views.generic import ListView
-from django.shortcuts import render, redirect
-from django.urls import reverse
+from django.views.generic import ListView, DetailView
 from . import models
 
 
@@ -16,27 +13,5 @@ class HomeView(ListView):
     context_object_name = "rooms"
 
 
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        return render(request, "rooms/detail.html", {"room": room})
-
-    except models.Room.DoesNotExist:
-        return redirect(reverse("core:home"))
-
-
-"""
-from math import ceil
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)
-
-    try:
-        rooms = paginator.page(page)
-        return render(request, "rooms/home.html", {"page": rooms})
-    except (EmptyPage, PageNotAnInteger):
-        return redirect("/")
-"""
+class RoomDetail(DetailView):
+    model = models.Room
