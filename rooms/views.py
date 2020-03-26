6ffa7ea5b90c0170763ea35bd1d677f42ae7afcc
jonggeunpark <53558710+jonggeunpark@users.noreply.github.com>
2020-03-26 23:59:40 +0900

    add form

--- rooms/views.py
+++ rooms/views.py
@@ -32,8 +32,13 @@ def search(request):
     beds = int(request.GET.get("beds", 0))
     bedrooms = int(request.GET.get("bedrooms", 0))
     baths = int(request.GET.get("baths", 0))
+    instant = request.GET.get("instant", False)
+    super_host = request.GET.get("super_host", False)
+
     s_amenities = request.GET.getlist("amenities")
     s_facilities = request.GET.getlist("facilities")
+    s_house_rules = request.GET.getlist("house_rules")
+
     form = {
         "city": city,
         "s_room_type": room_type,
@@ -43,18 +48,31 @@ def search(request):
         "beds": beds,
         "bedrooms": bedrooms,
         "baths": baths,
+        "instant": instant,
+        "super_host": super_host,
         "s_amenities": s_amenities,
         "s_facilities": s_facilities,
+        "s_house_rules": s_house_rules,
     }
 
     room_types = models.RoomType.objects.all()
     amenities = models.Amenity.objects.all()
     facilities = models.Facility.objects.all()
+    house_rules = models.HouseRule.objects.all()
 
     choices = {
         "room_types": room_types,
         "countries": countries,
         "amenities": amenities,
         "facilities": facilities,
+        "house_rules": house_rules,
     }
-    return render(request, "rooms/search.html", {**form, **choices},)
+
+    filter_args = {}
+
+    if city != "Anywhere":
+        filter_args["city"] = city
+
+    rooms = models.Room.objects.filter(**filter_args)
+
+    return render(request, "rooms/search.html", {**form, **choices}, {"rooms": rooms})
